7dcdc301e0baf34dff7ccd5c2567b84ae6ec4a33
Owen-sz <owen@fyralabs.com>
2024-08-15 16:11:45 -0500

    if statements and functionality update

--- fetch.sh
+++ fetch.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
+# This script uses both Bash and awk
 
-# OS
-source /etc/os-release
-echo $ID
+if [ -f ~/bin/deviceinfo ]; then
+    # OS
+    source /etc/os-release
+    echo OS Info: "$ID $VERSION $VERSION_CODENAME"
 
-# CPU
-grep -m 1 'model name' /proc/cpuinfo 
+#    CPU
+    grep -m 1 'model name' /proc/cpuinfo | awk -F: '{gsub(/^[ \t]+|[ \t]+$/, "", $2); print "cpu: "$2}'
 
-# GPU
-gpu=$(lspci | grep -i vga | awk '{for (i=5; i<=NF; i++) printf $i " "; print ""}')
-echo "GPU: $gpu"
+    # GPU
+    gpu=$(lspci | grep -i vga | awk '{for (i=5; i<=NF; i++) printf $i " "; print ""}')
+    echo "GPU: $gpu"
 
-# RAM
-memtotal=$(cat /proc/meminfo | grep -i memtotal | awk '{print $2/1024/1024 " GB"}')
-echo "Total Memory: $memtotal"
+    # RAM
+    memtotal=$(cat /proc/meminfo | grep -i memtotal | awk '{print $2/1024/1024 " GB"}')
+    echo "Total Memory: $memtotal"
 
-memavailable=$(cat /proc/meminfo | grep -i memavailable | awk '{print $2/1024/1024 " GB"}')
-echo "Available Memory: $memavailable"
+    memavailable=$(cat /proc/meminfo | grep -i memavailable | awk '{print $2/1024/1024 " GB"}')
+    echo "Available Memory: $memavailable"
 
-swaptotal=$(cat /proc/meminfo | grep -i swaptotal | awk '{print $2/1024/1024 " GB"}')
-echo "Total Swap: $swaptotal"
+    swaptotal=$(cat /proc/meminfo | grep -i swaptotal | awk '{print $2/1024/1024 " GB"}')
+    echo "Total Swap: $swaptotal"
 
-swapfree=$(cat /proc/meminfo | grep -i swapfree | awk '{print $2/1024/1024 " GB"}')
-echo "Swap Available: $swapfree"
+    swapfree=$(cat /proc/meminfo | grep -i swapfree | awk '{print $2/1024/1024 " GB"}')
+    echo "Swap Available: $swapfree"
 
-# disk
+    # disk
 
+    # Disk
+    # OS (name, releasever)
+    # Computer model (host)
+    # Network interface used
+    # Desktop Environment
+    # Kernel
+fi
 
-# CPU - =
-# GPU (i, d) - =
-# RAM =
-# Disk = 
-# OS (name, releasever) - =
-# Computer model (host)
-# Swap = 
-# Network interface used
-# Desktop Environment
-# Kernel
+# move script so it can be universally ran
+
+if [ ! -f ~/bin/deviceinfo ]; then
+    mkdir -p ~/bin
+    mv fetch.sh ~/bin/deviceinfo
+    chmod +x ~/bin/deviceinfo
+    echo 'export PATH=$PATH:~/bin' >> ~/.bashrc
+    source ~/.bashrc
+fi
