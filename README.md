f158c72c8b1459684a5d06351ae373339abd9eac
Owen-sz <owen@fyralabs.com>
2024-08-15 19:03:52 -0500

    Working script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Deviceinfo-fetch
-Fetch script to grab device hardware and OS information [WIP]
+Fetch script to grab device hardware and OS information
+
+### How to use
+
+Clone this repository
+`git clone https://github.com/Owen-sz/Deviceinfo-fetch.git`
+
+cd into the folder
+`cd Deviceinfo-fetch`
+
+Install the program
+`bash fetch.sh`
+
+Now, simply run `deviceinfo` from any directory. 
--- fetch.sh
+++ fetch.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 if [ -f ~/bin/deviceinfo ]; then
+
+    # Computer model (host)
+    hostname=$(hostname) 
+    echo Hostname: $hostname
+
     # OS
     source /etc/os-release
-    echo OS Info: "$ID $VERSION $VERSION_CODENAME"
+    echo OS: "$ID $VERSION $VERSION_CODENAME"
 
-#    CPU
-    grep -m 1 'model name' /proc/cpuinfo | awk -F: '{gsub(/^[ \t]+|[ \t]+$/, "", $2); print "cpu: "$2}'
+    # Kernel
+    kernel=$(uname -r)
+    echo Kernel: "$kernel"
+    
+    # CPU
+    grep -m 1 'model name' /proc/cpuinfo | awk -F: '{gsub(/^[ \t]+|[ \t]+$/, "", $2); print "CPU: "$2}'
 
     # GPU
     gpu=$(lspci | grep -i vga | awk '{for (i=5; i<=NF; i++) printf $i " "; print ""}')
@@ -17,22 +26,26 @@ if [ -f ~/bin/deviceinfo ]; then
     echo "Total Memory: $memtotal"
 
     memavailable=$(cat /proc/meminfo | grep -i memavailable | awk '{print $2/1024/1024 " GB"}')
-    echo "Available Memory: $memavailable"
+    echo "  Available Memory: $memavailable"
 
     swaptotal=$(cat /proc/meminfo | grep -i swaptotal | awk '{print $2/1024/1024 " GB"}')
-    echo "Total Swap: $swaptotal"
+    echo "  Total Swap: $swaptotal"
 
     swapfree=$(cat /proc/meminfo | grep -i swapfree | awk '{print $2/1024/1024 " GB"}')
-    echo "Swap Available: $swapfree"
+    echo "  Swap Available: $swapfree"
 
     # disk
+    echo "Disk info:"
+    root_fs_type=$(findmnt -n -o FSTYPE /)
+    disk=$(df / | awk 'NR==2 {print $1}' | sed 's/[0-9]*$//')
+    total_storage=$(lsblk -dn -o SIZE $disk)
+    available_space=$(df -h / | awk 'NR==2 {gsub("G", "GB", $4); print $4}')
+    type=$(lsblk -o TRAN | awk 'NR>1 {print $1}' | sort -u | paste -sd " ")
 
-    # Disk
-    # OS (name, releasever)
-    # Computer model (host)
-    # Network interface used
-    # Desktop Environment
-    # Kernel
+    echo "  Filesystem: $root_fs_type"
+    echo "  Total usable storage: $total_storage"
+    echo "  Available space: $available_space"
+    echo "  Type: $type"
 fi
 
 # move script so it can be universally ran
